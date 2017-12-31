1b87a0090b7e65e5149109b0017635da7177e7ac
Linus Miller <lohfu@lohfu.io>
2017-12-31 13:10:25 +0100

    Standard ESLint

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 /* eslint-disable no-console */
 
-'use strict';
+'use strict'
 
 const colorizeStack = require('colorize-stack')
-const chalk = require('chalk');
+const chalk = require('chalk')
 
-function extractLocation() {
-  const obj = {};
-  Error.captureStackTrace(obj);
-  const str = obj.stack.split(/\n\s*/)[3];
-  const cwd = process.cwd();
+function extractLocation () {
+  const obj = {}
+  Error.captureStackTrace(obj)
+  const str = obj.stack.split(/\n\s*/)[3]
+  const cwd = process.cwd()
 
   if (str.includes(cwd)) {
-    return str.slice(str.indexOf(cwd) + cwd.length + 1, -1);
+    return str.slice(str.indexOf(cwd) + cwd.length + 1, -1)
   }
 
-  return str.slice(str.indexOf('(') + 1, -1);
+  return str.slice(str.indexOf('(') + 1, -1)
 }
 
 module.exports = ({ obj = console, log, dir, error } = {}) => {
   if (log) {
-    obj.__log = obj.log;
+    obj.__log = obj.log
 
     obj.log = function (...args) {
-      const location = extractLocation();
+      const location = extractLocation()
 
       if (!location.includes('node_modules')) {
-        console.__log(chalk.grey(location));
+        console.__log(chalk.grey(location))
       }
 
-      console.__log(...args);
-    };
+      console.__log(...args)
+    }
   }
 
   if (dir) {
-    obj.__dir = obj.dir;
+    obj.__dir = obj.dir
 
     obj.dir = function (obj, options) {
-      console.__log(chalk.grey(extractLocation()));
+      console.__log(chalk.grey(extractLocation()))
       console.__dir(obj, Object.assign({
         colors: true,
-      }, options));
-    };
+      }, options))
+    }
   }
 
   if (error) {
-    obj.__error = obj.error;
+    obj.__error = obj.error
 
     obj.error = function (err, ...args) {
       if (err.stack) {
